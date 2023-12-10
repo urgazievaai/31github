157d3445282824452a5f0e5bbaf1ecb5893e338a
Urgazieva Aigerim <aigerim.urgazieva.on@gmail.com>
2023-12-11 02:01:10 +0600

    webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
   entry: {
-    filename: path.resolve(__dirname, 'src/index.js')
+    main: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name][contenthash].js',
-    assetModuleFilename: '[name][ext]'
+    assetModuleFilename: '[name][ext]',
+    publicPath: '/',
   },
   externals: {
     express: 'express',
@@ -18,21 +20,25 @@ module.exports = {
   performance: {
     hints: false,
     maxAssetSize: 512000,
-    maxEntrypointSize: 512000
+    maxEntrypointSize: 512000,
   },
   devServer: {
     port: 3000,
     compress: true,
     hot: true,
     static: {
-      directory: path.join(__dirname, 'dist')
-    }
+      directory: path.join(__dirname, 'dist'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(css)$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -42,27 +48,35 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'roboto-mono/[name].[ext]'
-        }
+          filename: 'roboto-mono/[name].[ext]',
+        },
       },
       {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-      },
       },
     ],
   },
   plugins: [
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       title: 'My web Page',
       filename: 'index.html',
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'], 
+      bootstrap: 'bootstrap' 
+    }),
+    
   ],
 };
