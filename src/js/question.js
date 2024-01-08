46b36d89f045ce6a7a49435470f56582b0e66157
Urgazieva Aigerim <aigerim.urgazieva.on@gmail.com>
2024-01-09 01:28:10 +0600

    moved firebase settings to firebase.config file

--- src/js/question.js
+++ src/js/question.js
@@ -1,18 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { app } from "./firebaseConfig";
 import { getDatabase, ref, onChildAdded } from "firebase/database"
+ 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB4omcKQ0OYERr2K-Ufd4xAmf_yl4o_V-U",
-  authDomain: "podcast-questions-19424.firebaseapp.com",
-  databaseURL:
-    "https://podcast-questions-19424-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "podcast-questions-19424",
-  storageBucket: "podcast-questions-19424.appspot.com",
-  messagingSenderId: "962804388469",
-  appId: "1:962804388469:web:c34ea30c7b7c65ddca9e3c",
-};  
-
-const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 export class Question {
@@ -60,7 +49,7 @@ let cureentQues = []
 function fromDb (questions) {
   console.log('это яяя')
   cureentQues = Object.values(questions || {})
-  console.log(cureentQues)
+  // console.log(cureentQues)
   return renderQues()
 // function getQuesDB(questions) {
   
